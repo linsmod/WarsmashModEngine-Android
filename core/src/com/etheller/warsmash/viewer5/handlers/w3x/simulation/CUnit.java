480ae3f31cbc0c2d90d2a9642b8d8ff575f29753
Tiny Rain <love@sandsea.info>
2023-07-19 15:29:44 +0800

    rebase

--- core/src/com/etheller/warsmash/viewer5/handlers/w3x/simulation/CUnit.java
+++ core/src/com/etheller/warsmash/viewer5/handlers/w3x/simulation/CUnit.java
@@ -1,6 +1,5 @@
 package com.etheller.warsmash.viewer5.handlers.w3x.simulation;
 
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
@@ -74,6 +73,7 @@ import com.etheller.warsmash.viewer5.handlers.w3x.simulation.trigger.enumtypes.C
 import com.etheller.warsmash.viewer5.handlers.w3x.simulation.unit.CUnitTypeJass;
 import com.etheller.warsmash.viewer5.handlers.w3x.simulation.unit.CWidgetEvent;
 import com.etheller.warsmash.viewer5.handlers.w3x.simulation.util.*;
+import com.google.code.appengine.awt.image.BufferedImage;
 
 public class CUnit extends CWidget {
 	private static RegionCheckerImpl regionCheckerImpl = new RegionCheckerImpl();
